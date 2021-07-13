4b4cf87830c91529c22203ec7fa6a6c6ce1f2770
limse10 <lse.01@hotmail.com>
2021-07-13 23:53:19 +0800

    add qrk api

--- Quark-API.js
+++ Quark-API.js
@@ -4,7 +4,7 @@ window.addEventListener(
   (event) => {
     if (event.data == "downloadPNG") {
       switchMode(0);
-      setTimeout(1,draw())
+      setTimeout(1, draw());
       let uri = getCanvasURI();
       let type = "downloadPNG";
       event.source.postMessage(
@@ -14,15 +14,33 @@ window.addEventListener(
         },
         event.origin
       );
-    } else {
-      return;
+    }
+    if (event.data.fn == "qrk_load_data") {
+      qrk_load_data(event.data.payload.data);
+    }
+    if (event.data.fn == "qrk_save_data") {
+      event.source.postMessage(
+        { fn: "qrk_save_data", payload: user_data },
+        event.origin
+      );
     }
   },
   false
 );
 
 function getCanvasURI() {
-  
   let URI = document.getElementById("defaultCanvas0").toDataURL();
   return URI;
 }
+
+const qrk_load_data = (data) => {
+  lines = data.lines;
+  fills = data.fills;
+  points = data.points;
+  tbs = data.tbs;
+  return true;
+};
+
+const qrk_save_data = () => {
+  return { lines: lines, fills: fills, points: points, tbs: tbs };
+};
